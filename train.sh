c62f72631b3f9d50be1b3f200d13a3ec359badbc
yinmh17 <31975170+yinmh17@users.noreply.github.com>
2020-04-04 01:31:44 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=0 python src/main.py --modelName "MS-Dual-Guided" --batch_size 8 --epochs 500 --lr 1e-3            
+CUDA_VISIBLE_DEVICES=0 python src/main.py --modelName "MS-Dual-Guided" --batch_size 8 --epochs 300 --lr 1e-3            
